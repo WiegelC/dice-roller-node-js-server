dd19293384ab6a3a0f46ed4defcddbc92b89d77c
Wiegelc <lckywgl81@outlook.com>
2025-03-02 13:38:12 -0600

    Remove unessaary file

--- index.js
+++ index.js
@@ -1,43 +1,12 @@
 const express = require('express')
-app = express()
-
+const app = express()
 const cors = require("cors")
 
-var url = require('url');
-var dt = require('./date-time');
-
 const port = process.env.PORT || 3000
-const majorVersion = 1
-const minorVersion = 3
 
-// Use Express to publish static HTML, CSS, and JavaScript files that run in the browser. 
+// Enable CORS and static file serving
 app.use(express.static(__dirname + '/static'))
 app.use(cors({ origin: '*' }))
-
-// The app.get functions below are being processed in Node.js running on the server.
-// Implement a custom About page.
-app.get('/about', (request, response) => {
-	console.log('Calling "/about" on the Node.js server.')
-	response.type('text/plain')
-	response.send('About Node.js on Azure Template.')
-})
-
-app.get('/version', (request, response) => {
-	console.log('Calling "/version" on the Node.js server.')
-	response.type('text/plain')
-	response.send('Version: '+majorVersion+'.'+minorVersion)
-})
-
-app.get('/api/ping', (request, response) => {
-	console.log('Calling "/api/ping"')
-	response.type('text/plain')
-	response.send('ping response')
-})
-
-// Create a route that serves the JSON data
-app.get('/api/favorite-places', (req, res) => {
-  res.json(favoritePlaces);
-});
 
 // Endpoint for rolling a dice
 app.get('/api/roll-dice', (request, response) => {
@@ -48,20 +17,20 @@ app.get('/api/roll-dice', (request, response) => {
 
 // Custom 404 page.
 app.use((request, response) => {
-  response.type('text/plain')
-  response.status(404)
-  response.send('404 - Not Found')
+    response.type('text/plain')
+    response.status(404)
+    response.send('404 - Not Found')
 })
 
 // Custom 500 page.
 app.use((err, request, response, next) => {
-  console.error(err.message)
-  response.type('text/plain')
-  response.status(500)
-  response.send('500 - Server Error')
+    console.error(err.message)
+    response.type('text/plain')
+    response.status(500)
+    response.send('500 - Server Error')
 })
 
 app.listen(port, () => console.log(
-  `Express started at \"http://localhost:${port}\"\n` +
-  `press Ctrl-C to terminate.`)
+    `Express started at \"http://localhost:${port}\"\n` +
+    `press Ctrl-C to terminate.`)
 )
